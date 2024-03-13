1321b9a9d87aa9ae5d6af6769944a32133a894a5
zhaoyachao <1209687056@qq.com>
2024-03-13 23:24:03 +0800

    v5.3.0优化启动脚本

--- label/bin/start.sh
+++ label/bin/start.sh
@@ -3,4 +3,4 @@ bin_path=`dirname "$0"`
 cd "$bin_path/.."
 pt=`pwd`
 APP_NAME=${pt}"/zdh_label.jar"
-nohup java -Dfile.encoding=utf-8 -Dloader.path=libs/,conf/ -Xss512M -jar "$APP_NAME"  >> label.log  &
+nohup java -Dfile.encoding=utf-8 -Dloader.path=libs/,conf/ -Xms512M -jar "$APP_NAME"  >> label.log  &
--- plugin/bin/start.sh
+++ plugin/bin/start.sh
@@ -2,4 +2,4 @@ set ff=unix
 bin_path=`dirname "$0"`
 cd "$bin_path/.."
 pt=`pwd`
-nohup java -Dfile.encoding=utf-8 -Dloader.path=libs/,conf/ -Xss512M -jar "${pt}/"zdh_plugin.jar >> plugin.log  &
+nohup java -Dfile.encoding=utf-8 -Dloader.path=libs/,conf/ -Xms512M -jar "${pt}/"zdh_plugin.jar >> plugin.log  &
